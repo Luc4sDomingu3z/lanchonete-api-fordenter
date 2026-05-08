8fc0652a0334037c797c9d16518a71cba6fe68ad
Luc4sDomingu3z <pereiraluccas68@gmail.com>
2026-05-08 09:23:22 -0300

    Servicos Dbcontext

--- Program.cs
+++ Program.cs
@@ -1,19 +1,32 @@
+using lanchonete_api.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddDbContext<LanchoneteApiDbContext>();
+
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-  app.MapOpenApi();
-  app.UseSwagger();
-  app.UseSwaggerUI(opt =>
-  {
-    opt.SwaggerEndpoint("/swagger/v1/swagger.json", "Lanchonete Api - Sprint ");
-  });
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(opt =>
+    {
+        opt.SwaggerEndpoint("/swagger/v1/swagger.json", "Lanchonete Api - Sprint ");
+    });
 }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
