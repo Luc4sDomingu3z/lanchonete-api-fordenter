f9d917bd452018e110bc2c2009c50c779bc7a24a
Luc4sDomingu3z <pereiraluccas68@gmail.com>
2026-05-12 11:42:43 -0300

    Renomeando arquivo data context

---
+++ Data/DataContext.cs
@@ -0,0 +1,32 @@
+using lanchonete_api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace lanchonete_api.Data;
+
+public class LanchoneteApiDbContext(DbContextOptions<LanchoneteApiDbContext> options) : DbContext(options)
+{
+
+    public DbSet<Operador>? Operadores { get; set; }
+    public DbSet<Pedido>? Pedidos { get; set; }
+    public DbSet<Cliente>? Clientes { get; set; }
+    public DbSet<Lanche>? Lanches { get; set; }
+    public DbSet<LancheSaida>? LancheSaidas { get; set; }
+    public DbSet<Bebida>? Bebidas { get; set; }
+    public DbSet<BebidaSaida>? BebidaSaidas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Lanche>()
+            .HasMany(l => l.Saidas)
+            .WithOne(s => s.Lanche)
+            .HasForeignKey(s => s.LancheId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        modelBuilder.Entity<Bebida>()
+            .HasMany(b => b.Saidas)
+            .WithOne(s => s.Bebida)
+            .HasForeignKey(s => s.BebidaId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+
+}
